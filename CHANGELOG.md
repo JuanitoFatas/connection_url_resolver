05fcee5f56c9252d6fb4dd61cb4b6e2d6721754d
Juanito Fatas <katehuang0320@gmail.com>
2018-02-05 02:34:12 +0900

    Release v1.0.0 (#3)

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Master
+
+## 1.0.0
+
+2018.02.05
+
+- first implementation #2 @JuanitoFatas
--- lib/connection_url_resolver/version.rb
+++ lib/connection_url_resolver/version.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ConnectionUrlResolver
-  VERSION = "0.0.0"
+  VERSION = "1.0.0"
 end
